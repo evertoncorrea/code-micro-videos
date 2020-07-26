f5c072e4a3b0a0649738e29c1432a5cc6c27c7e5
Everton Correa <eversc@yahoo.com.br>
2020-07-26 19:51:26 -0300

    criação teste API Category + outros ajustes

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Route::group(['namespace'=>'Api', 'as' => 'api'], function(){
 Route::group(['namespace'=>'Api'], function(){
     $exceptCreateAndEdit = ['except' => ['create', 'edit']];
     Route::resource('categories', 'CategoryController', $exceptCreateAndEdit);
